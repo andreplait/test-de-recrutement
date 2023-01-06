97b1b6d613eb4df4abec65220c13885a2dd762f0
Ascallen <72162502+AscallenHM@users.noreply.github.com>
2023-01-06 14:05:28 +0100

    Review
    En commentaire l.11 / l.31 / l.45 / l.55 / l.71

--- php/3-hard/Customer.php
+++ php/3-hard/Customer.php
@@ -8,7 +8,7 @@ namespace App;
 
 class Customer
 {
-    public function __construct(String $name)
+    public function __construct(String $name) //J'aurais déclaré rentals aussi dans le construct
     {
         $this->name = $name;
     }
@@ -28,7 +28,7 @@ class Customer
         $frequentRenterPoints = 0;
         $result = "Rental Record for " . $this->getName() . "\n";
 
-        foreach ($this->rentals as $each){
+        foreach ($this->rentals as $each){ //le nommage de la variable $each est pas compréhensible, $rental aurait été plus adapté.
            $thisAmount = 0.0;
 
            /* @var $each Rental */
@@ -42,7 +42,7 @@ class Customer
                case Movie::NEW_RELEASE:
                    $thisAmount += $each->getDaysRented() * 3;
                    break;
-               case Movie::CHILDREN:
+               case Movie::CHILDREN: //utiliser le case default
                    $thisAmount += 1.5;
                    if($each->getDaysRented() > 3) {
                        $thisAmount += ($each->getDaysRented() - 3) * 1.5;
@@ -52,8 +52,8 @@ class Customer
 
            $frequentRenterPoints++;
 
-           if($each->getMovie()->getPriceCode() == Movie::NEW_RELEASE
-                && $each->getDaysRented() > 1)
+           if($each->getMovie()->getPriceCode() == Movie::NEW_RELEASE //J'aurais placé ce code dans le switch pour éviter de faire un if prenant le même premier param
+                && $each->getDaysRented() > 1) // il manque les { }                                         dans le case NEW_RELASE un if ($each->getDaysRented() > 1
                $frequentRenterPoints++;
 
             $result .= "\t" . $each->getMovie()->getTitle() . "\t"
@@ -68,6 +68,6 @@ class Customer
         return $result;
     }
 
-    private string $name;
+    private string $name; // les déclarations en debut de class
     private array $rentals = [];
 }
