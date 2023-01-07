405f47e1bed104181cd6ce73dd8d930a385f041a
Ascallen <72162502+AscallenHM@users.noreply.github.com>
2023-01-07 14:26:10 +0100

    Review JS
    Commentaire l.2 / l.6

--- js/shadow-variable.js
+++ js/shadow-variable.js
@@ -1,9 +1,9 @@
 const articleList = []; // In a real app this list would be full of articles.
-var kudos = 5;
+var kudos = 5; //L'utilisation de let est plus safe
 
 function calculateTotalKudos(articles) {
   var kudos = 0;
-  
+  // pas de vérification si l'article a des kudos, risque de plantage si aucun
   for (let article of articles) {
     kudos += article.kudos;
   }
